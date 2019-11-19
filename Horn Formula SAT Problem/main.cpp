442bbd399d3cfc866c473135997b7f70495119ec
George <georgii.kashin@gmail.com>
2019-11-19 17:36:34 +0300

    Initial Commit

---
+++ Horn Formula SAT Problem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Horn Formula SAT Problem
+//
+//  Created by Георгий Кашин on 19.11.2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
